2082d50dacfdd7c15a53574aac21355a79d8144e
Sachvk <serg22011985@gmail.com>
2024-08-07 10:30:54 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,7 @@
+immutable_var = (1, 2, 3, 'Сергей', True)
+print(immutable_var)
+#immutable_var[0] = 5 #Попытка изменения кортежа
+mutable_list = ['Сергей', 'Денис', 'Андрей', 'Антон']
+print(mutable_list)
+mutable_list[1] = 'Дмитрий'
+print(mutable_list)
